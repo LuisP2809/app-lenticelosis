534de768b813f712ea6b894edfb505774e188742
Luis Alejandro Pineda Dávila <luisaleja2809@gmail.com>
2026-07-07 19:07:11 -0500

    Make offline cache tolerant for builds

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@ const APP_SHELL = [
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(APP_SHELL))
+      .then((cache) => Promise.allSettled(APP_SHELL.map((url) => cache.add(url))))
       .then(() => self.skipWaiting())
   );
 });
